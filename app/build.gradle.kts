257d615b99a2c9c174762bb9a40712331cc050d6
MicheleConnectReply <168536132+MicheleDep97@users.noreply.github.com>
2025-08-24 22:36:05 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,16 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 tasks.register<DokkaTask>("dokkaJson") {
     outputDirectory.set(layout.buildDirectory.dir("dokka/json"))
+    pluginsMapConfiguration.set(
+        mapOf(
+            "org.jetbrains.dokka.json.JsonFormatPlugin" to mapOf<String, String>()
+        )
+    )
 }
-
 
 android {
     namespace = "com.example.example_app"
